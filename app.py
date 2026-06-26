afa86add1e686933b09ce7220f439952b719748f
Karljoel <eraconceptstudios@gmail.com>
2026-06-26 11:20:44 -0700

    fix duplicate IDs and syntax in app.py

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Our list of matches (we'll add more later)
+# Our list of matches
 matches = [
-<<<<<<< HEAD
     {
         "id": 1,
         "home": "Morocco",
@@ -21,9 +20,7 @@ matches = [
         "league": "World Cup",
         "date": "2026-06-25",
         "time": "01:00",
-        "stream_link": "https://hesgoalss.hes-goals.mov/?m=4627881&p=87350",
-        "stream_link2": None,
-        "stream_link3": None
+        "stream_link": "https://hesgoalss.hes-goals.mov/?m=4627881&p=87350"
     },
     {
         "id": 3,
@@ -32,7 +29,7 @@ matches = [
         "league": "World Cup",
         "date": "2026-06-25",
         "time": "04:00",
-        "stream_link": None 
+        "stream_link": None
     },
     {
         "id": 4,
@@ -43,10 +40,8 @@ matches = [
         "time": "04:00",
         "stream_link": None
     },
-    {   "id": 5,
-=======
-    {   "id": 1,
->>>>>>> 1a0415a0a70b4cd3baeca7ff0a60f4ef0b5c543c
+    {
+        "id": 5,
         "home": "Curacao",
         "away": "Ivory Coast",
         "league": "World Cup",
@@ -54,7 +49,8 @@ matches = [
         "time": "23:00",
         "stream_link": None
     },
-    {   "id": 2,
+    {
+        "id": 6,
         "home": "Ecuador",
         "away": "Germany",
         "league": "World Cup",
@@ -62,7 +58,8 @@ matches = [
         "time": "23:00",
         "stream_link": None
     },
-    {   "id": 3,
+    {
+        "id": 7,
         "home": "Tunisia",
         "away": "Netherlands",
         "league": "World Cup",
@@ -70,38 +67,41 @@ matches = [
         "time": "02:00",
         "stream_link": None
     },
-    {   "id": 4,
+    {
+        "id": 8,
         "home": "Japan",
         "away": "Sweden",
         "league": "World Cup",
         "date": "2026-06-27",
         "time": "02:00",
         "stream_link": None
-     },
-     {   "id": 5,
+    },
+    {
+        "id": 9,
         "home": "Turkiye",
         "away": "USA",
         "league": "World Cup",
         "date": "2026-06-27",
         "time": "05:00",
         "stream_link": None
-     },
-     {   "id": 6,
+    },
+    {
+        "id": 10,
         "home": "Paraguay",
         "away": "Australia",
         "league": "World Cup",
         "date": "2026-06-27",
         "time": "05:00",
         "stream_link": None
-     }
+    }
 ]
+
 @app.route('/')
 def home():
     return render_template('index.html', matches=matches)
 
 @app.route('/match/<int:match_id>')
 def match_page(match_id):
-    # Find the match with this ID
     match = None
     for m in matches:
         if m['id'] == match_id:
